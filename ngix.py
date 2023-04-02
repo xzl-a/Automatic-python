1c03385df8b6d5bcc4df87bb62c89df002c41d57
xzl-a <2430351260@qq.com>
2023-04-02 07:14:52 -0400

    ngix.py update

--- ngix.py
+++ ngix.py
@@ -14,22 +14,19 @@ with open(nginx_file) as f:
         else:
             ip[s] = 1
             ip = sorted(ip.items(), key=lambda e: e[1], reverse=True)
-    print(ip)
     sorted_dict = sorted(ip.items(), key=lambda x: x[1], reverse=True)[:10]
     x=[]
     y=[]
     for i in sorted_dict:
-        print(f"{i[0]}    {i[1]}")
         x.append(i[0])
         y.append(i[1])       
-    print(x)
     plt.title('ip access')
     plt.xlabel('ip address')
     plt.ylabel('pv')
     plt.xticks(rotation=-30,fontsize=6)
 # 显示每个柱状图的值
-    for a, b, i in zip(x, y,range(len(x))):
-        plt.text(a, b, '%.0f' % y[i], ha='center', fontsize=6)
+    for a, b in zip(x, y):
+        plt.text(a, b, '%.0f' % b, ha='center', va='bottom', fontsize=6)
         plt.bar(x, y)
         plt.savefig('my_plot.png')
     
